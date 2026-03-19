3726795346f023a30804e00bc294ea044011075c
Sifflet_blanc <flo.crahay@gmail.com>
2026-03-19 15:06:33 +0100

    Séparation par airport

--- api/src/service/cleanData.py
+++ api/src/service/cleanData.py
@@ -7,7 +7,7 @@ def clean_data(data):
 	data['lightning_id'] = pd.to_numeric(data['lightning_id'])#, errors='coerce')
 	data['date'] = pd.to_datetime(data['date']).dt.tz_localize(None)
 
-	alerte = False
+	"""alerte = False
 	alerte_value = None
 	for i in range(len(data)):
 		if not pd.isna(data.at[i, 'airport_alert_id']) and not data.at[i, 'is_last_lightning_cloud_ground'] and alerte_value != data.at[i, 'airport_alert_id']:
@@ -16,7 +16,7 @@ def clean_data(data):
 		if alerte and data.at[i, 'is_last_lightning_cloud_ground']:
 			alerte = False
 		if alerte:
-			data.at[i, 'airport_alert_id'] = alerte_value
+			data.at[i, 'airport_alert_id'] = alerte_value"""
 
 def modify_data(data):	
 	data.insert(2, 'year', data['date'].dt.year)
@@ -44,16 +44,26 @@ def modify_data(data):
 							.mean()\
 							.reset_index(level=0, drop=True)['icloud'])
 
-	second_lighning = data[data["nst_of_storm"] == 2]
+	second_lighning = data[data["nst_of_storm"] == 2].copy()
 	second_lighning["original_index"] = second_lighning.index -1
-	#data.insert(16, "dist_derivative", (second_lighning["dist"] - data.loc[second_lighning["original_index"], "dist"]))# / ((data["date"] - data["date"].shift(1)) / pd.Timedelta(seconds=1)))
-	
+	"""data.insert(16, "dist_derivative", (second_lighning["dist"] - data.loc[second_lighning["original_index"], "dist"].values) / ((second_lighning["date"] -  data.loc[second_lighning["original_index"], "date"].values) / pd.Timedelta(seconds=1)))
+	data.insert(17, "cluster_dist", data.groupby('airport', group_keys=False)[['date', 'dist']]\
+							.rolling(5, closed = "right", min_periods=1)\
+							.rolling("1h", on="date", closed = "right", min_periods=1)\
+							.mean()\
+							.reset_index(level=0, drop=True)['dist'])"""
+
+
 	#Compte le nombre d'éclair precedent enregistré sur la même alerte
 	data.insert(18, "nst_of_alert", data.groupby((data['airport_alert_id'] != data['airport_alert_id'].shift())\
 										.cumsum())['airport_alert_id']\
       									.cumcount())
 
-	data.drop(['lightning_id', 'lightning_airport_id', 'date'], axis=1, inplace=True)
+	rep = {}
+	for airport in data["airport"].unique():
+		rep[str(airport)] = data[data["airport"] == airport].drop(['airport'], axis=1)
+
+	return rep
 	
 
 def process_data():
@@ -63,11 +73,12 @@ def process_data():
 	clean_data(data)
 	print("End of the clean process\n")
 	print("Start of the modifying process...")
-	modify_data(data)
+	dico = modify_data(data)
 	print("End of the modifying process\n")
 
 	print("Saving change...")
-	data.to_csv("bdd/segment_alerts_all_airports_train_clean.csv", index=False)
-	print("Clean data are now in 'bdd/segment_alerts_all_airports_train_clean.csv'")
+	for airport in dico:
+		dico.get(airport).to_csv("bdd/segment_alerts_"+airport+"_train_clean.csv", index=False)
+		print("Clean data are now in 'bdd/segment_alerts_"+airport+"_train_clean.csv'")
 
 process_data()
