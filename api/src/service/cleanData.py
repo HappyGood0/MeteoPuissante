2cfbe615541f5f9246dc69245853e049c01e81b5
Sifflet_blanc <flo.crahay@gmail.com>
2026-03-21 15:13:00 +0100

    Ajout du barycentre et de la vitesse moyenne de l'orage

--- api/src/service/cleanData.py
+++ api/src/service/cleanData.py
@@ -1,10 +1,29 @@
 import pandas as pd
-import numpy as np
+from tqdm import trange
+
+def barycentre(df, time_col, col, time_window, max_rows):
+	results = []
+
+	for j in trange(0, len(df), 2500, desc="Calc the barycentre of " + col):
+		for i in range(j, min(j+2500, len(df))):
+			current_time = df.iloc[i][time_col]
+
+			window_start = current_time - pd.Timedelta(time_window)
+
+			row_start = max(0, i - max_rows + 1)
+			window = df.iloc[row_start:i + 1]
+
+			window = window[window[time_col] >= window_start]
+
+			results.append(window[col].mean())
+
+	return pd.Series(results, index=df.index)
+
 
 def clean_data(data):
 	data.loc[data['is_last_lightning_cloud_ground'] != True, ['is_last_lightning_cloud_ground']] = False
 
-	data['lightning_id'] = pd.to_numeric(data['lightning_id'])#, errors='coerce')
+	data['lightning_id'] = pd.to_numeric(data['lightning_id'])
 	data['date'] = pd.to_datetime(data['date']).dt.tz_localize(None)
 
 	alerte = False
@@ -18,7 +37,9 @@ def clean_data(data):
 		if alerte:
 			data.at[i, 'airport_alert_id'] = alerte_value
 
-def modify_data(data):	
+def modify_data(data):
+	max_delta_of_same_storm = "1h"
+
 	data.insert(2, 'year', data['date'].dt.year)
 
 	#Le jour actuel dans l'année
@@ -26,42 +47,59 @@ def modify_data(data):
 
 	#La seconde actuelle dans la journée
 	data.insert(4, 'second', data['date'].dt.hour * 3600 + data['date'].dt.minute * 60 + data['date'].dt.second)
+	data['total_second'] = (data['date'] - pd.to_datetime("1/1/2000")).dt.total_seconds()
 
 	#Compte le nombre d'éclair precedent enregistré sur le même orage
 	data.insert(0, "nst_of_storm", data.groupby('airport', group_keys=False)[['date', 'lightning_id']]\
-							.rolling("1h", on="date", closed = "right", min_periods=1)\
+							.rolling(max_delta_of_same_storm, on="date", closed = "right", min_periods=1)\
 							.count()\
 							.reset_index(level=0, drop=True)['lightning_id'])
 
 	#fait la moyenne et variance des amplitudes des eclairs qui appartiennent au même orage (séparé deux à deux de moins d'une heure)
 	ampli_rolling = data.groupby('airport', group_keys=False)[['date', 'amplitude']]\
-							.rolling("1h", on="date", closed = "right", min_periods=1)
+							.rolling(max_delta_of_same_storm, on="date", closed = "right", min_periods=1)
 	data.insert(10, "mean_amplitude", ampli_rolling.mean().reset_index(level=0, drop=True)['amplitude'])
 	data.insert(11, "var_amplitude", ampli_rolling.var().reset_index(level=0, drop=True)['amplitude'])
 	
 	data.insert(14, "prop_icloud_of_storm", data.groupby('airport', group_keys=False)[['date', 'icloud']]\
-							.rolling("1h", on="date", closed = "right", min_periods=1)\
+							.rolling(max_delta_of_same_storm, on="date", closed = "right", min_periods=1)\
 							.mean()\
 							.reset_index(level=0, drop=True)['icloud'])
 
-	second_lighning = data[data["nst_of_storm"] == 2].copy()
-	second_lighning["original_index"] = second_lighning.index -1
-	"""data.insert(16, "dist_derivative", (second_lighning["dist"] - data.loc[second_lighning["original_index"], "dist"].values) / ((second_lighning["date"] -  data.loc[second_lighning["original_index"], "date"].values) / pd.Timedelta(seconds=1)))
-	data.insert(17, "cluster_dist", data.groupby('airport', group_keys=False)[['date', 'dist']]\
-							.rolling(5, closed = "right", min_periods=1)\
-							.rolling("1h", on="date", closed = "right", min_periods=1)\
-							.mean()\
-							.reset_index(level=0, drop=True)['dist'])"""
+	data.insert(16, 'dist_barycentre', barycentre(data, 'date', 'dist', time_window=max_delta_of_same_storm, max_rows=6))
+	data['date_barycentre'] = barycentre(data, 'date', 'total_second', time_window=max_delta_of_same_storm, max_rows=6)
+	data.insert(18, 'azimuth_barycentre', barycentre(data, 'date', 'azimuth', time_window=max_delta_of_same_storm, max_rows=6))
+
+	m = 3
+	data['dist_barycentre_diff'] = (
+		data.groupby('airport', group_keys=False)['dist_barycentre']
+		.diff()
+		.where(data['nst_of_storm'] > m, pd.NA)
+	)
+	data['date_barycentre_diff'] = (
+		data.groupby('airport', group_keys=False)['date_barycentre']
+		.diff()
+		.where(data['nst_of_storm'] > m, pd.NA)
+	)
+	data['azimuth_barycentre_diff'] = (
+		data.groupby('airport', group_keys=False)['azimuth_barycentre']
+		.diff()
+		.where(data['nst_of_storm'] > m, pd.NA)
+	)
+	#Vitesse en coordonnée carthesienne
+	data.insert(19, "barycentre_speed", (data["dist_barycentre_diff"] + data["dist_barycentre"] * data["azimuth_barycentre_diff"]) / data["date_barycentre_diff"].clip(lower=1))
+
+
 
 
 	#Compte le nombre d'éclair precedent enregistré sur la même alerte
-	data.insert(18, "nst_of_alert", data.groupby((data['airport_alert_id'] != data['airport_alert_id'].shift())\
+	data.insert(21, "nst_of_alert", data.groupby((data['airport_alert_id'] != data['airport_alert_id'].shift())\
 										.cumsum())['airport_alert_id']\
-      									.cumcount())
+	  									.cumcount())
 
 	rep = {}
 	for airport in data["airport"].unique():
-		rep[str(airport)] = data[data["airport"] == airport].drop(['airport', 'lightning_id', 'lightning_airport_id', 'date'], axis=1)
+		rep[str(airport)] = data[data["airport"] == airport].drop(['airport', 'lightning_id', 'lightning_airport_id', 'date', 'lat', 'lon', 'total_second', 'date_barycentre', 'dist_barycentre_diff', 'date_barycentre_diff', 'azimuth_barycentre_diff'], axis=1)
 
 	return rep
 	
