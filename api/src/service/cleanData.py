8b9f13e019620ae1704b5731d6e52ef10d4ef824
Sifflet_blanc <flo.crahay@gmail.com>
2026-03-25 17:06:01 +0100

    Plus de meteo france

--- api/src/service/cleanData.py
+++ api/src/service/cleanData.py
@@ -12,7 +12,7 @@ station_meteo_fr_id = {
 	"Nantes": 44020001,
 }
 
-token = "" # token meteo france
+token = "eyJ4NXQiOiJZV0kxTTJZNE1qWTNOemsyTkRZeU5XTTRPV014TXpjek1UVmhNbU14T1RSa09ETXlOVEE0Tnc9PSIsImtpZCI6ImdhdGV3YXlfY2VydGlmaWNhdGVfYWxpYXMiLCJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJVdGlsaXNhdGV1ckBjYXJib24uc3VwZXIiLCJhcHBsaWNhdGlvbiI6eyJvd25lciI6IlV0aWxpc2F0ZXVyIiwidGllclF1b3RhVHlwZSI6bnVsbCwidGllciI6IlVubGltaXRlZCIsIm5hbWUiOiJEZWZhdWx0QXBwbGljYXRpb24iLCJpZCI6Mzg3NDQsInV1aWQiOiI4MGQ5NWU5NC01ZjNjLTQ0YjgtODY3Yi00Mzc1MzExMTY2NWIifSwiaXNzIjoiaHR0cHM6XC9cL3BvcnRhaWwtYXBpLm1ldGVvZnJhbmNlLmZyOjQ0M1wvb2F1dGgyXC90b2tlbiIsInRpZXJJbmZvIjp7IjUwUGVyTWluIjp7InRpZXJRdW90YVR5cGUiOiJyZXF1ZXN0Q291bnQiLCJncmFwaFFMTWF4Q29tcGxleGl0eSI6MCwiZ3JhcGhRTE1heERlcHRoIjowLCJzdG9wT25RdW90YVJlYWNoIjp0cnVlLCJzcGlrZUFycmVzdExpbWl0IjowLCJzcGlrZUFycmVzdFVuaXQiOiJzZWMifX0sImtleXR5cGUiOiJQUk9EVUNUSU9OIiwic3Vic2NyaWJlZEFQSXMiOlt7InN1YnNjcmliZXJUZW5hbnREb21haW4iOiJjYXJib24uc3VwZXIiLCJuYW1lIjoiRG9ubmVlc1B1YmxpcXVlc0NsaW1hdG9sb2dpZSIsImNvbnRleHQiOiJcL3B1YmxpY1wvRFBDbGltXC92MSIsInB1Ymxpc2hlciI6ImFkbWluX21mIiwidmVyc2lvbiI6InYxIiwic3Vic2NyaXB0aW9uVGllciI6IjUwUGVyTWluIn1dLCJleHAiOjE3NzUwNTgxNjYsInRva2VuX3R5cGUiOiJhcGlLZXkiLCJpYXQiOjE3NzQ0NTMxNjYsImp0aSI6ImVlMDQzYjcxLWZiOTktNDQzNi1iNzJkLTAzM2E5OTVmOTY4OSJ9.HtKf2jMJnmNIYmzObsYKNFEAoB3mH3_X3C5PEAnJDHqZFNBQ0S-zqrwbTiJ00w__IeSZcHaz_wIeeo37v_WJFyWw3m3_GeRnttSNFk369DwIK00GoqwL5pJh8echltabvhyHSz1i2eKM7eJDBQqadmyxivjlrORBn30QoC5K-MqcXKGYzGWlCPS8gWI8nVat6KYu-Zff5ns-LjU0Gk5ik0_4aKf_8pO2GRPji2hUf3Rc9Khj0GAZfQ3hdo656M_DHhqLDzIAIVMH5UethbPZT_uhiVI4YpOG5fD6bLth_WlZMZfUZfzlGao8FFi7rqwdpYWsyN6hAnPvusEi5UCM2g==" # token meteo france
 
 def barycentre(df, time_col, col, time_window, max_rows):
 	results = []
@@ -56,7 +56,7 @@ def fetch_commande_id(bearer_token, station_id, start_date, end_date):
 	url = f"https://public-api.meteofrance.fr/public/DPClim/v1/commande-station/horaire?id-station={station_id}&date-deb-periode={start_date}T00:00:00Z&date-fin-periode={end_date}T00:00:00Z"
 	headers = {
 		"accept": "*/*",
-		"Authorization": f"Bearer {bearer_token}"
+		"apikey": f"{bearer_token}"
 	}
 	print(url)
 	response = requests.get(url, headers=headers)
@@ -71,7 +71,8 @@ def fetch_commande_id(bearer_token, station_id, start_date, end_date):
 def fetch_meteo_file(bearer_token, commande_id):
 	url = f"https://public-api.meteofrance.fr/public/DPClim/v1/commande/fichier?id-cmde={commande_id}"
 	headers = {
-		"Authorization": f"Bearer {bearer_token}"
+		"accept": "*/*",
+		"apikey": f"{bearer_token}"
 	}
 	print(url)
 	response = requests.get(url, headers=headers)
@@ -83,40 +84,44 @@ def fetch_meteo_file(bearer_token, commande_id):
 def fetch_meteo_data(bearer_token, station_id, start_date, end_date):
 	commande_id = fetch_commande_id(bearer_token, station_id, start_date, end_date)
 
-	time.sleep(20)
+	time.sleep(10)
 
 	csv_content = fetch_meteo_file(bearer_token, commande_id)
 	df = pd.read_csv(StringIO(csv_content), sep=";")  # Utilisez le bon séparateur si nécessaire
-	return df[["DATE", "PSTAT"]]
+	return df[["DATE", "PSTAT", "U", "RR1", "DRR1", "FF", "T", "N", "NBAS", "CL", "CM", "CH", "N1", "N2", "N3", "N4", "UV_INDICE"]]
 
 
 
-def add_meteo_fr_data(data):
+def add_meteo_fr_data(data, downlaod_data = False):
 	data["date_meteo_fr"] = data['date'].dt.year * 1000000 + data['date'].dt.month * 10000 + data['date'].dt.day * 100 + data['date'].dt.hour
 
-	"""meteo_data = pd.DataFrame()
-	for airport in data["airport"].unique():
-		if airport in station_meteo_fr_id:
-			mmin, mmax = data[data["airport"] == airport]['date'].dt.date.agg(["min", "max"])
-			dates = pd.date_range(start=mmin, end=mmax, freq="1Y")
-			old_date = mmin
+	if downlaod_data:
+		meteo_data = pd.DataFrame()
+		for airport in data["airport"].unique():
+			if airport in station_meteo_fr_id:
+				mmin, mmax = data[data["airport"] == airport]['date'].dt.date.agg(["min", "max"])
+				dates = pd.date_range(start=mmin, end=mmax, freq="1Y")
+				old_date = mmin
 
-			for date in dates.date:
-				meteo_data_tmp = fetch_meteo_data(token, station_meteo_fr_id[airport], old_date, date)
+				for date in dates.date:
+					meteo_data_tmp = fetch_meteo_data(token, station_meteo_fr_id[airport], old_date, date)
+					meteo_data_tmp["name_station"] = airport
+					meteo_data = pd.concat([meteo_data, meteo_data_tmp], ignore_index=True)
+					old_date = date
+
+				meteo_data_tmp = fetch_meteo_data(token, station_meteo_fr_id[airport], old_date, mmax)
 				meteo_data_tmp["name_station"] = airport
 				meteo_data = pd.concat([meteo_data, meteo_data_tmp], ignore_index=True)
-				old_date = date
 
-			meteo_data_tmp = fetch_meteo_data(token, station_meteo_fr_id[airport], old_date, mmax)
-			meteo_data_tmp["name_station"] = airport
-			meteo_data = pd.concat([meteo_data, meteo_data_tmp], ignore_index=True)
+		meteo_data.to_csv("bdd/meteo_data.csv", index=False)
+	else :
+		meteo_data = pd.read_csv("bdd/meteo_data.csv", na_values=['Empty', '', 'NaN', 'nan'])
 
 
-	meteo_data.to_csv("bdd/meteo_data.csv", index=False)"""
-	meteo_data = pd.read_csv("bdd/meteo_data.csv", na_values=['Empty', '', 'NaN', 'nan'])
 	meteo_data['PSTAT'] = pd.to_numeric(meteo_data['PSTAT'].astype(str).str.replace(",", ".").replace("nan", np.nan))
-
-	print(meteo_data)
+	meteo_data['RR1'] = pd.to_numeric(meteo_data['RR1'].astype(str).str.replace(",", ".").replace("nan", np.nan))
+	meteo_data['FF'] = pd.to_numeric(meteo_data['FF'].astype(str).str.replace(",", ".").replace("nan", np.nan))
+	meteo_data['T'] = pd.to_numeric(meteo_data['T'].astype(str).str.replace(",", ".").replace("nan", np.nan))
 			
 	data = data.merge(
 		meteo_data,
@@ -124,7 +129,6 @@ def add_meteo_fr_data(data):
 		right_on=["DATE", "name_station"],
 		how="left"
 	)
-	print(data)
 	return data
 
 
@@ -159,6 +163,7 @@ def modify_data(data, for_training):
 							.mean()\
 							.reset_index(level=0, drop=True)['icloud'])
 	
+	
 	data.insert(16, 'dist_barycentre', barycentre(data, 'date', 'dist', time_window=max_delta_of_same_storm, max_rows=6))
 	data['date_barycentre'] = barycentre(data, 'date', 'total_second', time_window=max_delta_of_same_storm, max_rows=6)
 	data.insert(18, 'azimuth_barycentre', barycentre(data, 'date', 'azimuth', time_window=max_delta_of_same_storm, max_rows=6))
@@ -179,6 +184,7 @@ def modify_data(data, for_training):
 		.diff()
 		.where(data['nst_of_storm'] > m, pd.NA)
 	)
+
 	#Vitesse en coordonnée carthesienne
 	data.insert(19, "barycentre_speed", (data["dist_barycentre_diff"] + data["dist_barycentre"] * data["azimuth_barycentre_diff"]) / data["date_barycentre_diff"].clip(lower=1))
 	
@@ -196,7 +202,7 @@ def modify_data(data, for_training):
 	if for_training:
 		drop_columns.pop(0)
 	
-	data.drop(drop_columns, axis=1)
+	data = data.drop(drop_columns, axis=1)
 
 
 	column_to_move = "is_last_lightning_cloud_ground"
